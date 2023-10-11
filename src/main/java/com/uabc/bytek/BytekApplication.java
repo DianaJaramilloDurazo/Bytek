b7755f4119085103ef7d4d0899d9458cea5d8822
Fernando Huerta <huerta.fernando@uabc.edu.mx>
2023-10-11 15:29:26 -0700

    Creado el proyecto

---
+++ src/main/java/com/uabc/bytek/BytekApplication.java
@@ -0,0 +1,13 @@
+package com.uabc.bytek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BytekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uabc/bytek/BytekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uabc.bytek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
