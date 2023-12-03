9eb0f0bdeb6dd75511c08ceb511bf9b0d5971da6
EdgarNH29 <133627803+EdgarNH29@users.noreply.github.com>
2023-12-02 20:29:45 -0800

    Boton y estilo filtrar

--- src/main/resources/static/js/Historial.js
+++ src/main/resources/static/js/Historial.js
@@ -2,3 +2,18 @@ function quitarModal(modalId){
   const modal = bootstrap.Modal.getInstance(modalId);
   modal.hide();
 }
+
+function activarFiltros()
+{
+	var div = document.getElementById("filtrosid");
+	var estiloCalculado = window.getComputedStyle(div);
+	var estado = estiloCalculado.display;
+	
+	if(estado === "none")
+	{
+		div.style.display = "flex";
+		divact = true;
+	}else{
+		div.style.display ="none";
+	}
+}
